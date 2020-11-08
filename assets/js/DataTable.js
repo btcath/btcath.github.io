fd71ffa399512dd85791513ddcfdd086ac990fa3
btcath <74141227+btcath@users.noreply.github.com>
2020-11-08 21:54:25 +0400

    Create DataTable.js

---
+++ assets/js/DataTable.js
@@ -0,0 +1,8 @@
+    $(document).ready(function () {
+        $('#btcath').DataTable({
+            "paging": false,
+            "info": false,
+            fixedHeader: true,
+            responsive: true
+        });
+    });
